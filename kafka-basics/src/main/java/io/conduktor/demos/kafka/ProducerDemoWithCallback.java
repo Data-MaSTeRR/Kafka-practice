5db599c0fe45f7fe2aea63f1bf5af6bd92826917
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-27 19:14:09 +0900

    Feat: 콜백 추가 및 배치사이즈에 따른 파티션 분배

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
@@ -28,29 +28,42 @@ public class ProducerDemoWithCallback {
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
 
+        // 한번에 보내는 메시지의 배치 사이즈 400b로 줄임 - 운영에서는 쓰면 안됨 kafka의 기본으로 쓰기 16kb
+        properties.setProperty("batch.size", "400");
+
         // create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        // create a Producer Record
-        ProducerRecord<String, String> producerRecord =
-                new ProducerRecord<>("demo-java", "Hello world");
+        for (int j=0; j<10; j++) {
+            for (int i=0; i<30; i++) {
+                // create a Producer Record
+                ProducerRecord<String, String> producerRecord =
+                        new ProducerRecord<>("demo-java", "Hello world");
 
-        // send data - 비동기 전송 + 콜백추가 (버퍼의 모든 메시지 전송을 보장하지 않음 -> flush의 필요성)
-        producer.send(producerRecord, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                // e -> callback()
-                if (e == null) { // record was successfully sent
-                    log.info("Received new metadata \n" +
-                            "Topic: " + recordMetadata.topic() + "\n" +
-                            "Partition: " + recordMetadata.partition() + "\n" +
-                            "Offset: " + recordMetadata.offset() + "\n" +
-                            "Timestamp: " + recordMetadata.timestamp());
-                } else {
-                    log.error("Error with producing", e);
-                }
+                // send data - 비동기 전송 + 콜백추가 (버퍼의 모든 메시지 전송을 보장하지 않음 -> flush의 필요성)
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        // e -> callback()
+                        if (e == null) { // record was successfully sent
+                            log.info("Received new metadata \n" +
+                                    "Topic: " + recordMetadata.topic() + "\n" +
+                                    "Partition: " + recordMetadata.partition() + "\n" +
+                                    "Offset: " + recordMetadata.offset() + "\n" +
+                                    "Timestamp: " + recordMetadata.timestamp());
+                        } else {
+                            log.error("Error with producing", e);
+                        }
+                    }
+                });
             }
-        });
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         // tell the producer to send all data and block until done -- sync / 프로듀서 내부의 전송 버퍼에 남은 메시지를 모두 전송하고 완료될 때까지 기다립니다.
         producer.flush();
