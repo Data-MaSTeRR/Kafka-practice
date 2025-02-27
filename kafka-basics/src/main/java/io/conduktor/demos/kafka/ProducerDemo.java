f056406ebc7df8678c1c0cb673ed70a67595f493
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-27 18:15:58 +0900

    Feat: producer 전 properties에서 localhost에서 Broker와 연결하는 알고리즘 추가

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -3,6 +3,8 @@ package io.conduktor.demos.kafka;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Properties;
+
 
 public class ProducerDemo {
 
@@ -10,6 +12,14 @@ public class ProducerDemo {
 
     public static void main(String[] args) {
         log.info("Hello world");
+
+        // create Producer Properties - Broker와 연결
+        Properties properties = new Properties();
+
+        // connect to localhost
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+
     }
 }
 
