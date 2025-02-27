8f5b2ab63ddd27547720fb88b970981703abcdf5
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-27 18:47:07 +0900

    Feat: producer 메세지 만들어서 비동기 단순전송, flush

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -1,6 +1,7 @@
 package io.conduktor.demos.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +28,19 @@ public class ProducerDemo {
 
         // create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a Producer Record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo-java", "Hello world");
+
+        // send data - 비동기 단순 전송 (버퍼의 모든 메시지 전송을 보장하지 않음 -> flush의 필요성)
+        producer.send(producerRecord);
+
+        // tell the producer to send all data and block until done -- sync / 프로듀서 내부의 전송 버퍼에 남은 메시지를 모두 전송하고 완료될 때까지 기다립니다.
+        producer.flush();
+
+        // flush and close the producer - 남은 메시지 전송이 완료되고, 관련 리소스가 정리
+        producer.close();
+
     }
 }
-
