46ff22c598413986dce2e5a692f786a8c89521cd
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-27 21:50:27 +0900

    Feat: consumer의 properties 만들고, consumer 만들기

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
@@ -0,0 +1,39 @@
+package io.conduktor.demos.kafka;
+
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer!");
+
+        String groupId = "my-java-application";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // connect to localhost - Broker와 연결
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // create consumer configs
+        // set producer properties - 바이트로 역직렬화
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        // none, earliest, latest
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        // create the Consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+    }
+}
