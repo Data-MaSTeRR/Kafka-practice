b3bd4acbaa474d31b4963f3d77c96ea623786f2d
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-27 22:32:39 +0900

    Feat: consumer가 1초마다 polling, offset을 기록하여 파티션에 commit

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
@@ -1,11 +1,14 @@
 package io.conduktor.demos.kafka;
 
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -18,7 +21,7 @@ public class ConsumerDemo {
         log.info("I am a Kafka Consumer!");
 
         String groupId = "my-java-application";
-        String topic = "demo-java"
+        String topic = "demo-java";
 
         // create Producer Properties
         Properties properties = new Properties();
@@ -40,5 +43,20 @@ public class ConsumerDemo {
         // subscribe to a topic
         consumer.subscribe(Arrays.asList(topic));
 
+        // poll for data
+        while (true) {
+
+            log.info("polling");
+
+            // kafka data 기다리는 시간 -> kafka 과부하 방지
+            ConsumerRecords<String,String> consumerRecords =
+                    consumer.poll(Duration.ofMillis(1000));
+
+            // for문에서 인자 하나씩이니, ConsumerRecord 단수형
+            for (ConsumerRecord<String, String> consumerRecord :consumerRecords) {
+                log.info("Key: " + consumerRecord.key() + ", Value: " + consumerRecord.value());
+                log.info("Partition: " + consumerRecord.partition() + ", Offset: " + consumerRecord.offset());
+            }
+        }
     }
 }
